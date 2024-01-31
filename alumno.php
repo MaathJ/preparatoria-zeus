4c7458c4e935725d52fe2aab1d5edb34e7f7cc88
Jhon Oblitas <134904618+Zxpty@users.noreply.github.com>
2024-01-31 18:21:15 -0500

    fix: responsive desing for tables and edit dataTables for print

--- alumno.php
+++ alumno.php
@@ -297,56 +297,63 @@ include('modales_alumno.php');
 
 
 
-        let table = new DataTable('#table_alumno', {
-            language: {
-                "lengthMenu": "Mostrar _MENU_ registros",
-                "zeroRecords": "No se encontraron resultados",
-                "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
-                "sSearch": "Buscar:",
-                "oPaginate": {
-                    "sFirst": "Primero",
-                    "sLast": "Último",
-                    "sNext": "Siguiente",
-                    "sPrevious": "Anterior"
-                },
-                "sProcessing": "Procesando...",
-            },
-            //para usar los botones   
-            responsive: "true",
-            dom: 'Bfrtilp',
-            buttons: [{
-                    extend: 'excelHtml5',
-                    text: '<i class="fa-regular fa-file-excel"></i> ',
-                    titleAttr: 'Exportar a Excel',
-                    // className: 'btn btn-success'
-                },
-                {
-                    extend: 'pdfHtml5',
-                    text: '<i class="fa-regular fa-file-pdf"></i>',
-                    titleAttr: 'Exportar a PDF',
-                    exportOptions: {
-                        columns: [1, 2, 3, 4, 5]
+        $(document).ready(function() {
+            var table = $('#table_alumno').DataTable({
+                responsive: true,
+                language: {
+                    "lengthMenu": "Mostrar _MENU_ registros",
+                    "zeroRecords": "No se encontraron resultados",
+                    "info": " _TOTAL_ registros",
+                    "infoEmpty": "No hay registros para mostrar",
+                    "infoFiltered": "(filtrado de _MAX_  registros)",
+                    "sSearch": "Buscar:",
+                    "oPaginate": {
+                        "sFirst": "Primero",
+                        "sLast": "Último",
+                        "sNext": "Siguiente",
+                        "sPrevious": "Anterior"
                     },
-                    customize: function(doc) {
+                    "sProcessing": "Cargando...",
+                },
+                dom: 'Bfrtilp',
+                buttons: [{
+                        extend: 'excelHtml5',
+                        autofilter: true,
+                        text: '<i class="fa-regular fa-file-excel"></i>',
+                        titleAttr: 'Exportar a Excel',
+                        exportOptions: {
+                            columns: [1, 2, 3, 4, 5]
+                        }
 
-                        doc.content[1].table.body[0].forEach(function(h) {
-                            h.fillColor = 'rgb(1, 1, 51)';
-                        });
-                        doc.content[1].margin = [100, 0, 100, 0]
                     },
-                },
-                {
-                    extend: 'print',
-                    text: '<i class="fa-solid fa-print"></i>',
-                    titleAttr: 'Imprimir',
-                    // className: 'btn btn-info'
-                },
-            ]
+                    {
+                        extend: 'pdfHtml5',
+                        text: '<i class="fa-regular fa-file-pdf"></i>',
+                        titleAttr: 'Exportar a PDF',
+                        exportOptions: {
+                            columns: [1, 2, 3, 4, 5]
+                        },
+                        customize: function(doc) {
 
+                            doc.content[1].table.body[0].forEach(function(h) {
+                                h.fillColor = 'rgb(1, 1, 51)';
+                            });
+                        },
+                    },
+                    {
+                        extend: 'print',
+                        text: '<i class="fa-solid fa-print"></i>',
+                        titleAttr: 'Imprimir',
+                        exportOptions: {
+                            columns: [1, 2, 3, 4, 5]
+                        },
+
+                    },
+                ]
+            });
+
+            new $.fn.dataTable.FixedHeader(table);
         });
-
         function obtenerCarreras(idArea) {
             console.log(idArea);
             $.ajax({
