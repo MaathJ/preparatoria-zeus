bb2fccbb7e68ff26a81ee5903bedbcf4ac08db57
Jhon Oblitas <134904618+Zxpty@users.noreply.github.com>
2024-02-03 23:05:18 -0500

    se elimino un link que no se utiliza

--- boleta.php
+++ boleta.php
@@ -1,9 +1,9 @@
 <?php
 include_once('auth.php');
 include_once('config/conexion.php');
+include_once('src/components/parte_superior.php');
 include_once('app/controllers/boleta/Modal_boleta.php');
 include_once('app/controllers/pago/Modal_pago.php');
-include_once('src/components/parte_superior.php');
 $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
 ?>
 <link rel="stylesheet" href="src/assets/css/boleta/forma_pago.css">
